9b05b03c181b103a318f6341c3357dca7bd19b1d
junaidsss785 <junaidsss785@gmail.com>
2026-09-23 19:42:55 +0300

    Create element_card.dart

---
+++ lib/widgets/element_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../models/element_model.dart';
+
+class ElementCard extends StatelessWidget {
+  final ElementModel element;
+  final VoidCallback onTap;
+
+  const ElementCard({
+    super.key,
+    required this.element,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFF1E2530),
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: element.themeColor.withOpacity(0.4), width: 1.5),
+        ),
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(element.iconSymbol, style: const TextStyle(fontSize: 24)),
+            const SizedBox(height: 6),
+            Text(
+              element.name,
+              textAlign: TextAlign.center,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 12,
+              ),
+            ),
+            Text(
+              element.formula,
+              style: TextStyle(
+                color: element.themeColor,
+                fontSize: 11,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
